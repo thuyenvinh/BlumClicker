c6bc3f40f1eaaff7db6bc4b9c02747d8ae2ff0f0
Thuyen Nguyen <thuyenvinh@gmail.com>
2024-07-21 09:46:24 +0700

    Sửa và bổ

--- Form1.cs
+++ Form1.cs
@@ -42,18 +42,20 @@ namespace BlumClickWinForm
         private void buttonHelp_Click(object sender, System.EventArgs e)
         {
             _toolTipStart.Show(
-                "Нажмите CTRL для остановки бота." +
-                "\nПолзунки служат для изменения параметров захвата изображения." +
-                "\nСначала разместите окно BLum в центра экрана для корректной работы." +
-                "\nЗатем нажмите кнопку Play в Blum. Далее нажмите кнопку Start в программе." +
-                "\nGame Auto Run отвечает за автоматическое нажатие кнопки Play конце игры в Blum." +
-                "\n\nЧем больше область тем меньше производительность!!!",
-                buttonRun);
+      "Nhấn CTRL để dừng bot." +
+      "\nCác thanh trượt dùng để thay đổi các thông số chụp ảnh." +
+      "\nĐầu tiên, hãy đặt cửa sổ BLum ở trung tâm màn hình để hoạt động chính xác." +
+      "\nSau đó nhấn nút Play trong Blum. Tiếp theo nhấn nút Start trong chương trình." +
+      "\nGame Auto Run chịu trách nhiệm tự động nhấn nút Play cuối game trong Blum." +
+      "\n\nKhu vực càng lớn thì hiệu suất càng giảm!!!",
+      buttonRun);
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Process.Start("https://t.me/CoderWorker");
+           
+            Process.Start(new ProcessStartInfo("https://t.me/CoderWorker") { UseShellExecute = true });
+            Process.Start(new ProcessStartInfo("https://t.me/BlumCryptoBot/app?startapp=ref_tgXDajsgLo") { UseShellExecute = true });
         }
 
         private void checkBoxAutoRun_CheckedChanged(object sender, System.EventArgs e)
